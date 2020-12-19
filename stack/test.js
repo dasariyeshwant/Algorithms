3e2bede1b60b356e550842314fb524ce447bbfed
Yeshwant Dasari <ydasari@ford.com>
2020-12-18 22:02:59 -0500

    Creates Stack Class

---
+++ stack/test.js
@@ -0,0 +1,35 @@
+import Stack from "./index";
+it('Stack is a class', () => {
+  expect(typeof Stack.prototype.constructor).toEqual('function');
+});
+
+it('stack can add and remove items', () => {
+  const s = new Stack();
+  s.push(1);
+  expect(s.pop()).toEqual(1);
+  s.push(2);
+  expect(s.pop()).toEqual(2);
+});
+
+it('stack can follows first in, last out', () => {
+  const s = new Stack();
+  s.push(1);
+  s.push(2);
+  s.push(3);
+  expect(s.pop()).toEqual(3);
+  expect(s.pop()).toEqual(2);
+  expect(s.pop()).toEqual(1);
+});
+
+it('peek returns the first element but doesnt pop it', () => {
+  const s = new Stack();
+  s.push(1);
+  s.push(2);
+  s.push(3);
+  expect(s.peek()).toEqual(3);
+  expect(s.pop()).toEqual(3);
+  expect(s.peek()).toEqual(2);
+  expect(s.pop()).toEqual(2);
+  expect(s.peek()).toEqual(1);
+  expect(s.pop()).toEqual(1);
+});
