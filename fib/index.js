995eb96736932cd8a0ab9e9ff12142640da10b19
Yeshwant Dasari <ydasari@ford.com>
2020-12-20 17:02:15 -0500

    Implements memoize recuresive solution to fib

--- fib/index.js
+++ fib/index.js
@@ -19,10 +19,26 @@
 // };
 
 // Solution 2
-const fib = (n) => {
+const slowFib = (n) => {
   if (n < 2) {
     return n;
   }
   return fib(n - 1) + fib (n - 2);
 };
+
+// Memoize the fib
+const memoize = (fn) => {
+  const cache = {};
+  return (...args) => {
+    if (cache[args]) {
+      return cache[args];
+    }
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  }
+};
+const fib = memoize(slowFib);
+
+
 export default fib;
